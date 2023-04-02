21149836a2d22caa82776b76d85e81cfa4c0baa7
Anshu Kumar <anshu.ac.dv@gmail.com>
2023-04-02 16:47:00 +0530

    Initial commit

--- Employee/Dashboard.php
+++ Employee/Dashboard.php
@@ -5,59 +5,78 @@
   <title>Employee Dashboard</title>
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <!-- Custom CSS -->
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Employee Dashboard</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Dashboard</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Attendance</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Apply for Leave</a>
-        </li>
-      </ul>
-    </div>
+  <!-- Side header -->
+  <div class="side-header">
+    <h1>Employee Dashboard</h1>
+  </div>
+
+  <!-- Side navbar -->
+  <nav class="side-navbar">
+    <ul>
+      <li><a href="#">Dashboard</a></li>
+      <li><a href="#">Update Profile</a></li>
+      <li><a href="#">Attendance</a></li>
+      <li><a href="#">Apply for Leave</a></li>
+    </ul>
   </nav>
 
-  <!-- Dashboard -->
-  <div class="container my-4">
-    <h1>Dashboard</h1>
-    <p>Welcome to your employee dashboard.</p>
-  </div>
+  <!-- Main content -->
+  <div class="main-content">
+    <!-- Dashboard -->
+    <div class="container my-4">
+      <h1>Dashboard</h1>
+      <p>Welcome to your employee dashboard.</p>
+    </div>
 
-  <!-- Attendance -->
-  <div class="container my-4">
-    <h1>Attendance</h1>
-    <p>You have attended 20 out of 22 working days this month.</p>
-  </div>
+    <!-- Update Profile -->
+    <div class="container my-4">
+      <h1>Update Profile</h1>
+      <form>
+        <div class="form-group">
+          <label for="fullName">Full Name</label>
+          <input type="text" class="form-control" id="fullName">
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
 
-  <!-- Apply for Leave -->
-  <div class="container my-4">
-    <h1>Apply for Leave</h1>
-    <form>
-      <div class="form-group">
-        <label for="startDate">Start Date</label>
-        <input type="date" class="form-control" id="startDate">
-      </div>
-      <div class="form-group">
-        <label for="endDate">End Date</label>
-        <input type="date" class="form-control" id="endDate">
-      </div>
-      <div class="form-group">
-        <label for="reason">Reason</label>
-        <textarea class="form-control" id="reason" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <!-- Attendance -->
+    <div class="container my-4">
+      <h1>Attendance</h1>
+      <p>You have attended 20 out of 22 working days this month.</p>
+    </div>
+
+    <!-- Apply for Leave -->
+    <div class="container my-4">
+      <h1>Apply for Leave</h1>
+      <form>
+        <div class="form-group">
+          <label for="startDate">Start Date</label>
+          <input type="date" class="form-control" id="startDate">
+        </div>
+        <div class="form-group">
+          <label for="endDate">End Date</label>
+          <input type="date" class="form-control" id="endDate">
+        </div>
+        <div class="form-group">
+          <label for="reason">Reason</label>
+          <textarea class="form-control" id="reason" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
   </div>
 
   <!-- Bootstrap JS -->
